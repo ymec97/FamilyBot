c5e5e3027d1d800d39673c4800bea8bb78dc5f48
YMeC <ymec97@gmail.com>
2020-08-08 21:00:12 +0300

    Commands: Added report command. implemented problems classes

--- main.py
+++ main.py
@@ -1,21 +1,74 @@
 #!/usr/bin/python3
 import sys
 from time import sleep
-from telegram.ext import Updater
 import logging
-from telegram.ext import MessageHandler, Filters, CommandHandler
+from telegram.ext import MessageHandler, Filters, CommandHandler, Updater
 
+from datetime import date
 
 # For debug purposes
 import pdb
 
 
-problems = ["שטויות", "בלאגן"]
+MAX_SUPPORTED_OPEN_PROBLEMS = 100
+MAX_SUPPORTED_FIXED_ISSUES = 100
+DATE_FORMAT = "%d/%m/%Y"
+
+class Problem():
+	def __init__(self, id, description):
+		self.id = id
+		self.is_active = True
+		self.description = description
+		self.date_opened = date.today().strftime(DATE_FORMAT)
+		self.date_closed = None # None until closed
+		self.date_fixed = None # None until fixed
+
+	def _today(self):
+		return date.today().strftime(DATE_FORMAT)
+
+	def fix(self):
+		self.date_fixed = self._today
+		self.is_active = False
+
+class Problems():
+	problems = {}
+	fixed_issues = {}
+
+
+	def __init__(self):
+		for id in range(MAX_SUPPORTED_OPEN_PROBLEMS):
+			self.problems[id] = None
+
+	def _getFreeId(self):
+		# Supporting up to 100 problems
+		for id in self.problems.keys():
+			if not self.problems[id]:
+				return id
+
+	def newProblem(self, description):
+		newId = self._getFreeId()
+		self.problems[newId] = Problem(id, description)
+
+	def fixProblem(self, id):
+		self.problems[id].fix()
+		self.fixed_issues[id] = self.problems[id]
+		self.problems[id] = None
+
+	def delProblem(self, id):
+		if self.problems[id] == None:
+			return False
+
+		self.problems[id] = None
+		return True
+
+probs = Problems()
 
 def getToken():
 	""" Read bot token from file """
 	return open("APIs/Telegram/CohenFamilyBot/token.txt").read()
 
+
+# Command logics
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
@@ -23,8 +76,14 @@ def status(update, context):
 	""" Handle /status command in the bot """
 	context.bot.send_message(chat_id=update.effective_chat.id, text='\n'.join(problems))
 
+def report(update, context):
+	""" Handle /report command in the bot """
+	#context.bot.send_message(chat_id=update.effective_chat.id, text='\n'.join(problems))
+	probs.newProblem(''.join(context.args))
+
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="נו חלאס.")
+
 
 def main():
 	logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -37,15 +96,17 @@ def main():
 	# Handlers
 	start_handler = CommandHandler('start', start)
 	status_handler = CommandHandler('status', status)
+	report_handler = CommandHandler('report', report)
 	unknown_handler = MessageHandler(Filters.command, unknown)
 
 
 	dispatcher.add_handler(start_handler)
 	dispatcher.add_handler(status_handler)
+	dispatcher.add_handler(report_handler)
 	dispatcher.add_handler(unknown_handler)
 	
 	updater.start_polling()
-	
+	updater.idle()
 
 if __name__ == '__main__':
     main()    
